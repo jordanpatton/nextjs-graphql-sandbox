4a74c51a3e74edf8362a89480a3f4647d05ee244
Jordan Patton <jordanpatton@gmail.com>
2020-06-17 17:20:08 -0700

    example query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
-const Home: React.FC = () => <div>Home</div>;
+import withApollo from '../lib/withApollo';
 
-export default Home;
+const EXAMPLE_GQL = gql`
+    query GetContinents {
+        continents {
+            name
+        }
+    }
+`;
+
+const Index: React.FC = () => {
+    const { data, error, loading } = useQuery(EXAMPLE_GQL);
+    if (loading) return <pre>Loading...</pre>;
+    if (error) return <pre>{JSON.stringify(error, null, 4)}</pre>;
+    return <pre>{JSON.stringify(data, null, 4)}</pre>;
+};
+
+export default withApollo(Index);
